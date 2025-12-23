d8d918d7590f3632d54274b121adcc37664aafcb
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-12-24 01:01:14 +0300

    feat: add page parsing response

--- config/env.go
+++ config/env.go
@@ -34,7 +34,7 @@ func NewDatabaseConfig() *DataBaseConfig {
 func NewLogConfig() *LogConfig {
 	return &LogConfig{
 		Level:  getInt("LOG_LEVEL", 0),
-		Format: getString("LOG_FORMAT", ""),
+		Format: getString("LOG_FORMAT", "json"),
 	}
 }
 
--- internal/home/handler.go
+++ internal/home/handler.go
@@ -1,19 +1,22 @@
 package home
 
 import (
+	"bytes"
+	"html/template"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/rs/zerolog"
 )
 
 type HomeHandler struct {
 	router       fiber.Router
-	customLogger zerolog.Logger
+	customLogger *zerolog.Logger
 }
 
 func NewHandler(router fiber.Router, customLogger *zerolog.Logger) {
 	h := &HomeHandler{
 		router:       router,
-		customLogger: *customLogger,
+		customLogger: customLogger,
 	}
 	api := h.router.Group("/api")
 	api.Get("/", h.home)
@@ -21,10 +24,24 @@ func NewHandler(router fiber.Router, customLogger *zerolog.Logger) {
 }
 
 func (h *HomeHandler) home(c *fiber.Ctx) error {
-	return c.SendString("Hello")
+	tmpl := template.Must(template.ParseFiles("./html/page.html"))
+	data := struct{ Count int }{Count: 1}
+
+	var tpl bytes.Buffer
+
+	if err := tmpl.Execute(&tpl, data); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, "Template compile error")
+	}
+
+	c.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
+	return c.Send(tpl.Bytes())
 }
 
 func (h *HomeHandler) error(c *fiber.Ctx) error {
-	h.customLogger.Info()
+	h.customLogger.Info().
+		Bool("isAdmin", true).
+		Str("email", "a@a.ru").
+		Int("id", 10).
+		Msg("INFO")
 	return fiber.NewError(fiber.StatusBadRequest, "Limit params is undefined")
 }
